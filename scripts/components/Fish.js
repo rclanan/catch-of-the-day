066f12ab9fcfee423474e393cd85fc9a7e970899
Ray Clanan <rclanan@utopianconcept.com>
2015-12-11 14:43:02 -0500

    Finished Wes Bos React-For-Beginners course

---
+++ scripts/components/Fish.js
@@ -0,0 +1,36 @@
+/*
+  Fish
+  <Fish />
+*/
+import React from 'react';
+import h from '../helpers';
+import autobind from 'autobind-decorator';
+
+@autobind
+class Fish extends React.Component {
+  onButtonClick() {
+    console.log('Going to add the fish: ', this.props.index);
+    let key = this.props.index;
+    this.props.addToOrder(key);
+  }
+
+  render() {
+    let details = this.props.details;
+    let isAvailable = (details.status === 'available' ? true : false);
+    let buttonText = (isAvailable ? 'Add to Order' : 'Sold Out!');
+
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{h.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
+      </li>
+    );
+  }
+}
+
+export default Fish;
